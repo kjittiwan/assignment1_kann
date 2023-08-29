cca3fb9fed151f839e9133e35e8c6dae184fbbea
Kann Jittiwan <kjittiwan2@gmail.com>
2023-08-29 18:15:03 +0700

    randomize function and add select

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'antd';
+import { Button, Select } from 'antd';
 import './styles/App.scss';
 
 // Define a type for the state setter function
@@ -17,76 +17,101 @@ const App: React.FC = () => {
 
   // Function to move items in the array forward
   const moveItemsForward = (arr: string[], setArr: SetArrayState) => {
-    // Get the last item in the array
     const lastItem = arr[arr.length - 1];
     if (lastItem) {
-      // Create a new array with the last item moved to the front
       const newArr = [lastItem, ...arr.slice(0, arr.length - 1)];
-      // Update the state with the new array
       setArr(newArr);
     }
   };
 
   // Function to move items in the array backwards
   const moveItemsBackwards = (arr: string[], setArr: SetArrayState) => {
-    // Get the first item in the array
     const firstItem = arr[0];
     if (firstItem) {
-      // Create a new array with the first item moved to the end
       const newArr = [...arr.slice(1), firstItem];
-      // Update the state with the new array
       setArr(newArr);
     }
   };
 
   // Function to switch the positions of the first and second halves of the array (swap rows in the grid)
   const switchArrayHalves = (arr: string[], setArr: SetArrayState) => {
-    // Calculate the middle index of the array
     const middleIndex = Math.floor(arr.length / 2);
-    // Slice the array into two halves
     const firstHalf = arr.slice(0, middleIndex);
     const secondHalf = arr.slice(middleIndex);
-    // Concatenate the second half with the first half to switch their positions
     setArr([...secondHalf, ...firstHalf]);
   };
 
+  // Function to randomize the positions of items in the array
+  const randomizeArrayOrder = (arr: string[], setArr: SetArrayState) => {
+    const newArray = [...arr];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    setArr(newArray);
+  };
+
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
   return (
     <section>
       <header>
+        <Select
+          defaultValue="en"
+          onChange={handleChange}
+          options={[
+            { value: 'en', label: 'En' },
+            { value: 'th', label: 'Th' },
+          ]}
+          className='select'
+        />
         <h1>Layout & Style</h1>
-        <div>En</div>
+
       </header>
       <main>
         <div className="button-container">
           {/* Button to move items backwards */}
           <Button
             onClick={() => moveItemsBackwards(items, setItems)}
-            className="button"
+            className="action-button"
           >
             <div className="triangle-left" />
+            <div className='button-description'>
+              <span>Move shape</span>
+            </div>
           </Button>
           {/* Button to switch array halves (swapping rows in the grid) */}
           <Button
             onClick={() => switchArrayHalves(items, setItems)}
-            className="button"
+            className="action-button"
           >
             <div className="triangle-upright" />
             <div className="triangle-downward" />
+            <div className='button-description'>
+              <span>Move position</span>
+            </div>
           </Button>
           {/* Button to move items forward */}
           <Button
             onClick={() => moveItemsForward(items, setItems)}
-            className="button"
+            className="action-button"
           >
             <div className="triangle-right" />
+            <div className='button-description'>
+              <span>Move shape</span>
+            </div>
           </Button>
         </div>
         <div className="grid-container">
           {/* Render each item in the array */}
-          {items.map((item, index) => (
-            <div key={item} className={`grid-item-${index}`}>
-              <div className={item} />
-            </div>
+          {items.map((item, index, array) => (
+            <button
+            key={item}
+            onClick={() => {randomizeArrayOrder(array, setItems)}}
+            className={`grid-item-${index}`}>
+              <div className={`${item}`} />
+            </button>
           ))}
         </div>
       </main>
